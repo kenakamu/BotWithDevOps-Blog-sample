618b0363c276a2e8cd13ac5db9361305f34d8bf7
Kenichiro Nakamura <ten_eight_bones@hotmail.com>
2017-06-22 11:52:30 +0900

    article2
    This is a sample code for article [Create Bot for Microsoft Graph with DevOps 2: Get your appointment from Microsoft Graph and OAuth 2.0 with AuthBot](https://blogs.msdn.microsoft.com/kenakamu/2017/06/21/devops-with-bot-framework-chat-for-business-application-2-en/)

---
+++ article2/O365Bot/Dialogs/RootDialog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using AuthBot;
+using System.Configuration;
+using AuthBot.Dialogs;
+using System.Threading;
+using O365Bot.Services;
+
+namespace O365Bot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var message = await result as Activity;
+
+            // Check authentication
+            if (string.IsNullOrEmpty(await context.GetAccessToken(ConfigurationManager.AppSettings["ActiveDirectory.ResourceId"])))
+            {
+                // Run authentication dialog
+                await context.Forward(new AzureAuthDialog(ConfigurationManager.AppSettings["ActiveDirectory.ResourceId"]), this.ResumeAfterAuth, message, CancellationToken.None);
+            }
+            else
+            {
+                // get events
+                GraphService service = new GraphService(context);
+                var events = await service.GetEvents();
+                foreach (var @event in events)
+                {
+                    await context.PostAsync($"{@event.Start.DateTime}-{@event.End.DateTime}: {@event.Subject}");
+                }
+            }
+        }
+
+        private async Task ResumeAfterAuth(IDialogContext context, IAwaitable<string> result)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
