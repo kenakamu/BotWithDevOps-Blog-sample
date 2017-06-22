55742dafb401912aea1d903438d2868f03c83eb2
Kenichiro Nakamura <ten_eight_bones@hotmail.com>
2017-06-22 11:55:08 +0900

    article3
    This is a sample code for article [Create Bot for Microsoft Graph with DevOps 3: Unit Testing with Moq, AutoFac and Fakes](https://blogs.msdn.microsoft.com/kenakamu/2017/06/22/devops-with-bot-framework-chat-for-business-application-3-en/)

---
+++ article3/O365Bot/Services/IEventService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+
+namespace O365Bot.Services
+{
+    public interface IEventService
+    {
+        Task<List<Event>> GetEvents();
+    }
+}
