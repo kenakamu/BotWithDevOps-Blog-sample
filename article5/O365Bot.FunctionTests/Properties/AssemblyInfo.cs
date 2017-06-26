93d5e9daa1ecdc805cf8e5bd17265308802e416c
Kenichiro Nakamura <ten_eight_bones@hotmail.com>
2017-06-26 14:09:26 +0900

    article5
    This is a sample code for article [Create Bot for Microsoft Graph with DevOps 5: Function Test](https://blogs.msdn.microsoft.com/kenakamu/2017/06/26/devops-with-bot-framework-chat-for-business-application-5-en/)

---
+++ article5/O365Bot.FunctionTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("O365Bot.FunctionTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("O365Bot.FunctionTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("699d5130-8ecd-45ae-80bb-d9c096d94b2f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
