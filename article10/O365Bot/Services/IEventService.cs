fd493079533f3e44f3bbfc10ffee1d0e9dab136c
Kenichiro Nakamura <ten_eight_bones@hotmail.com>
2017-07-05 15:05:24 +0900

    article10
    This is a sample code for article [Create Bot for Microsoft Graph with DevOps 10: BotBuilder features – FormFlow 201 FormBuilder](https://blogs.msdn.microsoft.com/kenakamu/2017/07/05/devops-with-bot-…pplication-10-en/)

---
+++ article10/O365Bot/Services/IEventService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+
+namespace O365Bot.Services
+{
+    public interface IEventService
+    {
+        Task<List<Event>> GetEvents();
+        Task CreateEvent(Event @event);
+    }
+}
